8083090713641f87e55e77161386d0f4de86786a
jonasc <github@bcdf.eu>
2020-12-28 12:31:45 +0100

    Update: only look at files not already present

--- update.sh
+++ update.sh
@@ -12,21 +12,28 @@ function join_by { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d
 pattern="^\./\\($(join_by "\\|" "${folders[@]}")\\)"
 
 tempfile="$(mktemp)"
-trap 'rm -rf "$tempfile"' EXIT
+tempfile2="$(mktemp)"
+tempfile3="$(mktemp)"
+trap 'rm -rf "$tempfile" "$tempfile2" "$tempfile3"' EXIT
 
 # wget: Download index file
-# funzip: Unzip the compressed file
+# gunzip: Unzip the compressed file
 # cut: Remove the first two columns (timestamp and file size)
 # grep: Only used files in wanted folders
 wget --output-document=- --quiet https://cdn.media.ccc.de/INDEX.gz \
 | gunzip \
 | cut --delimiter=" " --fields=3- \
 | grep "$pattern" \
+| sort \
 > "$tempfile"
 
+find | sort > "$tempfile2"
+
+comm -23 "$tempfile" "$tempfile2" > "$tempfile3"
+
 # Add all URLs without space to annex
-sed --quiet 's@^\./\(\S\+\)$@https://cdn.media.ccc.de/\1 \1@p' "$tempfile" | git annex addurl --relaxed --raw --batch --with-files --jobs=cpus
-grep '\s' "$tempfile" | while IFS= read path
+sed --quiet 's@^\./\(\S\+\)$@https://cdn.media.ccc.de/\1 \1@p' "$tempfile3" | git annex addurl --relaxed --raw --batch --with-files --jobs=cpus
+grep '\s' "$tempfile3" | while IFS= read path
 do
 	if ! [[ -h "$path" || -f "$path" ]]
 	then
